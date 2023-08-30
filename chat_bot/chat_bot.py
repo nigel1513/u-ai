192445e4a29d451a35b763af5d7945151450ac32
유정현 <123634868+nigel1513@users.noreply.github.com>
2023-08-30 22:20:13 +0900

    Update chat_bot.py

--- chat_bot/chat_bot.py
+++ chat_bot/chat_bot.py
@@ -82,7 +82,7 @@ def chat_bot():
         
         #response = bardapi.core.Bard(API_KEY).get_answer(prompt)
 
-        bard = Bard(token=API_KEY, timeout=10)
+        bard = Bard(token_from_browser=True, timeout=10)
         response = bard.get_answer(prompt)
         #['choices'][0]['content'][0]
         st.write(response)
