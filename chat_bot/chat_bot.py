b2f8c6762e6fa92c31c52c9fa01d7a368a3c8669
유정현 <123634868+nigel1513@users.noreply.github.com>
2023-08-30 21:15:13 +0900

    Update chat_bot.py

--- chat_bot/chat_bot.py
+++ chat_bot/chat_bot.py
@@ -10,12 +10,12 @@ import os
 
 def chat_bot():
     
-    API_KEY = st.secrets["__Secure-1PSID"][0]
+    API_KEY = st.secrets["__Secure-1PSID"][0].replace("sidts-", "")
     API_KEY2 = st.secrets["__Secure-1PSIDTS"][0]
     API_KEY3 = st.secrets["__Secure-1PSIDCC"][0]
 
     cookie_dict = {"__Secure-1PSID":API_KEY, "__Secure-1PSIDTS":API_KEY2, "__Secure-1PSIDCC":API_KEY3}
-
+    
     #session = requests.Session()
     #session.headers = SESSION_HEADERS
     #session.cookies.set("__Secure-1PSID", cookie_dict["__Secure-1PSID"]) 
@@ -27,6 +27,7 @@ def chat_bot():
     session.cookies.set("__Secure-1PSIDCC", cookie_dict["__Secure-1PSIDCC"])
     # many models use triple hash '###' for keywords, Vicunas are simpler:
 
+    st.write(API_KEY, API_KEY2, API_KEY3)
     st.write(cookie_dict)
     st.write(cookie_dict["__Secure-1PSID"], cookie_dict["__Secure-1PSIDTS"], cookie_dict["__Secure-1PSIDCC"])
     
