4a5f0120694332a439a5ea1d87ef3659279d2e96
유정현 <123634868+nigel1513@users.noreply.github.com>
2023-08-30 21:29:16 +0900

    Update chat_bot.py

--- chat_bot/chat_bot.py
+++ chat_bot/chat_bot.py
@@ -79,10 +79,10 @@ def chat_bot():
         st.session_state.message.append({'role': "user", 'content': prompt})
         with st.chat_message("user"):
             st.write(prompt)
-        #bard = BardCookies(cookie_dict=cookie_dict, session=session, conversation_id=st.session_state.conversation_id)
+        bard = BardCookies(cookie_dict=cookie_dict, session=session, conversation_id=st.session_state.conversation_id)
         
-        response = bardapi.core.Bard(API_KEY).get_answer(prompt)
-        #bard.get_answer(prompt)
+        #response = bardapi.core.Bard(API_KEY).get_answer(prompt)
+        rsponse = bard.get_answer(prompt)
         st.write(response)
         #['choices'][0]['content'][0]
 
